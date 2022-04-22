5741f5210b285888ff304bbf659896fc40f3b435
Hitalo <hitalo331@outlook.com>
2022-04-22 13:40:00 -0300

    about: using buttons for links

--- gojira/modules/about.py
+++ gojira/modules/about.py
@@ -20,23 +20,23 @@ async def about(bot: Gojira, union: Union[CallbackQuery, Message]):
     message = union.message if is_callback else union
     lang = union._lang
 
-    kwargs: Dict = {}
+    keyboard = [
+        [
+            (lang.github_button, "https://github.com/HitaloSama/Gojira", "url"),
+            (lang.group_button, "https://t.me/SpamTherapy", "url"),
+        ]
+    ]
 
     is_private = await filters.private(bot, message)
     if is_private and is_callback:
-        keyboard = [
+        keyboard.append(
             [
                 (lang.back_button, "start"),
             ],
-        ]
-        kwargs["reply_markup"] = ikb(keyboard)
+        )
 
     await (message.edit_text if is_callback else message.reply_text)(
-        lang.about_text.format(
-            bot_name=bot.me.first_name,
-            github="<a href='https://github.com/HitaloSama/Gojira'>GitHub</a>",
-            group=f"<a href='https://t.me/SpamTherapy'>{lang.group}</a>",
-        ),
+        lang.about_text.format(bot_name=bot.me.first_name),
         disable_web_page_preview=True,
-        **kwargs,
+        reply_markup=ikb(keyboard),
     )
