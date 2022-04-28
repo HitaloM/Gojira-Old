c999d594d44dd14b8cfd37008f8c2563195a9715
Hitalo <hitalo331@outlook.com>
2022-04-28 18:54:27 -0300

    utils.langs: return "en" string if `KeyError`

--- gojira/utils/langs/core.py
+++ gojira/utils/langs/core.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2020 Cezar H. <https://github.com/usernein>
 
 import html
+from typing import Dict, List
 
 
 class LangsFormatMap(dict):
@@ -10,15 +11,7 @@ class LangsFormatMap(dict):
             self.used.append(key)
             if type(self.get(key)) is str:
                 return html.escape(self.get(key))
-        else:
-            return self.__missing__(key)
-
-    def __missing__(self, key):
-        if self.debug:
-            print(
-                f"Key '{key}' is missing on the string '{self.string}', language '{self.code}'"
-            )
-        return "{" + key + "}"
+        return str("{" + key + "}")
 
 
 class LangString(str):
@@ -26,27 +19,16 @@ class LangString(str):
         mapping = LangsFormatMap(**kwargs)
         mapping.string = self.key
         mapping.code = self.code
-        mapping.debug = self.debug
-        mapping.used = []
         formatted = self.format_map(mapping)
-
-        not_used = [key for key in kwargs if key not in mapping.used]
-        if self.debug and len(not_used):
-            for key in not_used:
-                print(
-                    f"The parameter '{key}' was passed to the string '{self.key}' (language '{self.code}') but was not used"
-                )
-            print("")
         return formatted
 
 
 class Langs:
-    strings = {}
-    available = []
-    code = "en"
-    debug = True
+    strings: Dict = {}
+    available: List = []
+    code: str = "en"
 
-    def __init__(self, strings=None, debug=True, **kwargs):
+    def __init__(self, strings: Dict = None, **kwargs):
         if strings is None:
             strings = {}
         self.strings = strings
@@ -60,23 +42,20 @@ class Langs:
             self.strings[language_code] = strings_object
             self.strings[language_code].update({"LANGUAGE_CODE": language_code})
 
-        # self.strings = {'en':{'start':'Hi {name}!'}}
         self.available = list(self.strings.keys())
         self.code = "en" if "en" in self.available else self.available[0]
-        self.debug = debug
 
     def __getattr__(self, key):
         try:
             result = self.strings[self.code][key]
-        except BaseException:
-            result = key
+        except KeyError:
+            result = self.strings["en"][key]
         obj = LangString(result)
         obj.key = key
         obj.code = self.code
-        obj.debug = self.debug
         return obj
 
-    def get_language(self, language_code):
+    def get_language(self, language_code: str):
         lang_copy = Langs(strings=self.strings)
         lang_copy.code = language_code
         return lang_copy
