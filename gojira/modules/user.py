f1657d6bb33f3f92c6e5237c0b2297f33a094f69
Hitalo <hitalo331@outlook.com>
2022-04-27 22:03:10 -0300

    user: added search system

--- gojira/modules/user.py
+++ gojira/modules/user.py
@@ -1,54 +1,116 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2022 Hitalo <https://github.com/HitaloSama>
 
+import asyncio
 import time
 from datetime import datetime
+from typing import Union
 
 import anilist
 from pyrogram import filters
 from pyrogram.helpers import ikb
-from pyrogram.types import Message
+from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 from gojira.bot import Gojira
 from gojira.utils.langs.decorators import use_chat_language
 
 
 @Gojira.on_message(filters.cmd(r"user (.+)"))
+@Gojira.on_callback_query(filters.regex(r"^user (\d+)\s?(\d+)?\s?(\d+)?"))
 @use_chat_language()
-async def user_view(bot: Gojira, message: Message):
-    lang = message._lang
-    query = message.matches[0].group(1)
+async def user_view(bot: Gojira, union: Union[Message, CallbackQuery]):
+    is_callback = isinstance(union, CallbackQuery)
+    message = union.message if is_callback else union
+    user = union.from_user
+    lang = union._lang
+
+    is_private = await filters.private(bot, message)
+
+    query = union.matches[0].group(1)
+
+    if is_callback:
+        user_id = union.matches[0].group(2)
+        if user_id is not None:
+            user_id = int(user_id)
+
+            if user_id != user.id:
+                return
+
+        is_search = union.matches[0].group(3)
+        if bool(is_search) and not is_private:
+            await message.delete()
 
     if not bool(query):
         return
 
     async with anilist.AsyncClient() as client:
-        try:
-            user = await client.get(query, "user")
-        except TypeError:
+        if not is_callback:
+            results = await client.search(query, "user", page=1, limit=10)
+            if results is None:
+                await asyncio.sleep(0.5)
+                results = await client.search(query, "user", page=1, limit=10)
+
+            if results is None or len(results) == 0:
+                return
+
+            if len(results) == 1:
+                query = results[0].name
+            else:
+                keyboard = []
+                for result in results:
+                    keyboard.append([(result.name, f"user {result.name} {user.id} 1")])
+                await message.reply_text(
+                    lang.search_results_text(
+                        query=query,
+                    ),
+                    reply_markup=ikb(keyboard),
+                )
+        else:
+            query = str(query)
+
+        auser = await client.get(query, "user")
+
+        if auser is None:
             return
 
-        if user is None:
-            return
+        text = f"<b>{auser.name}</b> (<code>{auser.id}</code>)\n\n"
+        text += f"<b>{lang.anime_stats}</b>:\n"
+        text += (
+            f"{lang.total_anime_watched}: <code>{auser.statistics.anime.count}</code>\n"
+        )
+        text += f"{lang.total_episode_watched}: <code>{auser.statistics.anime.episodes_watched}</code>\n"
+        text += f"{lang.total_time_spent}: <code>{auser.statistics.anime.minutes_watched}</code>\n"
+        text += f"{lang.average_score}: <code>{auser.statistics.anime.mean_score}</code>\n\n"
+        text += f"<b>{lang.manga_stats}</b>:\n"
+        text += (
+            f"{lang.total_manga_read}: <code>{auser.statistics.manga.count}</code>\n"
+        )
+        text += f"{lang.total_chapters_read}: <code>{auser.statistics.manga.chapters_read}</code>\n"
+        text += f"{lang.total_volumes_read}: <code>{auser.statistics.manga.volumes_read}</code>\n"
+        text += f"{lang.average_score}: <code>{auser.statistics.manga.mean_score}</code>\n\n"
+        text += f"<b>{lang.created_at}</b>: <code>{datetime.fromtimestamp(auser.created_at.timestamp)}</code>\n"
+        text += f"<b>{lang.updated_at}</b>: <code>{datetime.fromtimestamp(auser.updated_at.timestamp)}</code>"
 
-    text = f"<b>{user.name}</b> (<code>{user.id}</code>)\n\n"
-    text += f"<b>{lang.anime_stats}</b>:\n"
-    text += f"{lang.total_anime_watched}: <code>{user.statistics.anime.count}</code>\n"
-    text += f"{lang.total_episode_watched}: <code>{user.statistics.anime.episodes_watched}</code>\n"
-    text += f"{lang.total_time_spent}: <code>{user.statistics.anime.minutes_watched}</code>\n"
-    text += f"{lang.average_score}: <code>{user.statistics.anime.mean_score}</code>\n\n"
-    text += f"<b>{lang.manga_stats}</b>:\n"
-    text += f"{lang.total_manga_read}: <code>{user.statistics.manga.count}</code>\n"
-    text += f"{lang.total_chapters_read}: <code>{user.statistics.manga.chapters_read}</code>\n"
-    text += f"{lang.total_volumes_read}: <code>{user.statistics.manga.volumes_read}</code>\n"
-    text += f"{lang.average_score}: <code>{user.statistics.manga.mean_score}</code>\n\n"
-    text += f"<b>{lang.created_at}</b>: <code>{datetime.fromtimestamp(user.created_at.timestamp)}</code>\n"
-    text += f"<b>{lang.updated_at}</b>: <code>{datetime.fromtimestamp(user.updated_at.timestamp)}</code>"
+        keyboard = [[("🐢 Anilist", auser.url, "url")]]
 
-    keyboard = [[("🐢 Anilist", user.url, "url")]]
+        photo = f"https://img.anili.st/user/{auser.id}?a={time.time()}"
 
-    await message.reply_photo(
-        photo=f"https://img.anili.st/user/{user.id}?a={time.time()}",
-        caption=text,
-        reply_markup=ikb(keyboard),
-    )
+        if bool(message.photo) and is_callback:
+            await union.edit_message_media(
+                InputMediaPhoto(
+                    photo,
+                    caption=text,
+                ),
+                reply_markup=ikb(keyboard),
+            )
+        elif bool(message.photo) and not bool(message.via_bot):
+            await message.edit_text(
+                text,
+                reply_markup=ikb(keyboard),
+            )
+        else:
+            await message.reply_photo(
+                photo,
+                caption=text,
+                reply_markup=ikb(keyboard),
+            )
