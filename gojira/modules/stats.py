1a01436f11f5b9c0fa0dce3744a8df328cd26c34
Hitalo <hitalo331@outlook.com>
2022-04-29 20:04:32 -0300

    stats: added chats and users separated by language

--- gojira/modules/stats.py
+++ gojira/modules/stats.py
@@ -15,12 +15,17 @@ from pyrogram.types import Message
 from gojira.bot import Gojira
 from gojira.config import DATABASE_PATH
 from gojira.database import database
+from gojira.database.chats import filter_chats_by_language
+from gojira.database.users import filter_users_by_language
+from gojira.utils.langs.decorators import use_chat_language
 
 conn = database.get_conn()
 
 
 @Gojira.on_message(filters.cmd(r"stats$") & filters.sudo)
+@use_chat_language()
 async def stats_view(bot: Gojira, message: Message):
+    lang = message._lang
     text = "\n<b>Database</b>"
     text += f"\n    <b>Size</b>: <code>{humanize.naturalsize(os.stat(DATABASE_PATH).st_size, binary=True)}</code>"
     disk = shutil.disk_usage("/")
@@ -31,9 +36,15 @@ async def stats_view(bot: Gojira, message: Message):
     users_count = await conn.execute("select count() from users")
     users_count = await users_count.fetchone()
     text += f"\n    <b>Users</b>: <code>{users_count[0]}</code>"
+    for language in lang.strings.keys():
+        users = await filter_users_by_language(language=language)
+        text += f"\n        <b>{language.upper()}</b>: <code>{len(users)}</code>"
     groups_count = await conn.execute("select count() from chats")
     groups_count = await groups_count.fetchone()
     text += f"\n    <b>Groups</b>: <code>{groups_count[0]}</code>"
+    for language in lang.strings.keys():
+        groups = await filter_chats_by_language(language=language)
+        text += f"\n        <b>{language.upper()}</b>: <code>{len(groups)}</code>"
     text += "\n<b>System</b>"
     uname = platform.uname()
     text += f"\n    <b>OS</b>: <code>{uname.system}</code>"
