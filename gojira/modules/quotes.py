da71b7924a83d0dd4c7aca5edeee8d3b76bb881d
Hitalo <hitalo331@outlook.com>
2022-05-06 19:59:19 -0300

    quotes: added a 'reload' button

--- gojira/modules/quotes.py
+++ gojira/modules/quotes.py
@@ -1,19 +1,27 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2022 Hitalo <https://github.com/HitaloSama>
 
+from typing import Union
+
 import httpx
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.helpers import ikb
+from pyrogram.types import CallbackQuery, Message
 
 from gojira.bot import Gojira
 from gojira.utils.langs.decorators import use_chat_language
 
 
 @Gojira.on_message(filters.cmd(r"quote$"))
+@Gojira.on_callback_query(filters.regex(r"^quote$"))
 @use_chat_language()
-async def quote_message(bot: Gojira, message: Message):
-    lang = message._lang
-    sent = await message.reply_text(lang.getting_quote)
+async def quote_message(bot: Gojira, union: Union[Message, CallbackQuery]):
+    is_callback = isinstance(union, CallbackQuery)
+    message = union.message if is_callback else union
+    lang = union._lang
+
+    if not is_callback:
+        sent = await message.reply_text(lang.getting_quote)
 
     async with httpx.AsyncClient(http2=True) as client:
         response = await client.get("https://animechan.vercel.app/api/random")
@@ -23,7 +31,9 @@ async def quote_message(bot: Gojira, message: Message):
         data = response.json()
         await client.aclose()
 
-    text = f"<code>{data['quote']}</code>\n\n"
+    text = f"<code>❝{data['quote']}❞</code>\n\n"
     text += f"  ~ <b>{data['character']}</b> (<i>{data['anime']}</i>)"
 
-    await sent.edit_text(text)
+    button = [[("🔁", "quote")]]
+
+    await (message if is_callback else sent).edit_text(text, reply_markup=ikb(button))
